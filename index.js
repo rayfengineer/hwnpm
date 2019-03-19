46a7c792e3e929f267e8cd32e26b52cdb6494643
Ray Feng <rfeng@ospreysoftware.com>
2019-03-19 13:12:05 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+module.export = function(nums, k) {
+	let swapLen = nums.length - (k % nums.length);
+	swap(nums, 0, swapLen - 1);
+	swap(nums, swapLen, nums.length - 1);
+	swap(nums, 0, nums.length - 1);
+	console.log('Hello world', nums);
+	return nums;
+};
+
+const swap = (arr, start, end) => {
+	for (let i = start, j = end; i < j; i++, j--) {
+		let temp = arr[j];
+		arr[j] = arr[i];
+		arr[i] = temp;
+	}
+};
